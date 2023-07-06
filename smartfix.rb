1091fd0e4a45e12e0787ebda6c29c048c88615b5
Miles Luders <miles.luders@appfolio.com>
2023-07-06 16:27:53 -0700

    Remove deprecated bottle option

--- smartfix.rb
+++ smartfix.rb
@@ -6,7 +6,6 @@ class Smartfix < Formula
   url "https://github.com/mluders/smartfix/archive/v1.0.1.tar.gz"
   sha256 "4aeca4b667c97278f4b9ceb6fc3bfc812ef8fa1c3ca892aa65a22623c8f5fe0c"
   license ""
-  bottle :unneeded
 
   def install
     bin.install "src/smartfix" => "smartfix"
